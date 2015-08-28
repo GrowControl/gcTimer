cb57544aa5469afdc363efe46296edc17e0e74b1
lorenzop <lorenzo@poixson.com>
2015-08-27 21:08:27 -0400

    added PluginDefines class

---
+++ src/com/growcontrol/plugins/gctimer/PluginDefines.java
@@ -0,0 +1,17 @@
+package com.growcontrol.plugins.gctimer;
+
+
+public final class PluginDefines {
+	private PluginDefines() {}
+
+
+	// plugin config
+	public static final String CONFIG_FILE = "gcTimer.yml";
+	// config keys
+	public static final String CONFIG_VERSION = "Version";
+	// timers
+	public static final String CONFIG_TIMERS  = "Timers";
+	public static final String CONFIG_TIMER_ENABLED = "Enabled";
+
+
+}
