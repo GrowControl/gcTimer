bec68c21dbd9b0f38c2aa9c70dafb534cb299fd0
Lop <lop@lorenzo.lophome.com>
2015-10-22 16:37:23 -0400

    added getName() and toString() functions to xEventListener classes

--- src/com/growcontrol/plugins/gctimer/server/commands/Commands.java
+++ src/com/growcontrol/plugins/gctimer/server/commands/Commands.java
@@ -7,6 +7,18 @@ import com.poixson.commonjava.xLogger.commands.xCommandListener;
 
 public class Commands implements xCommandListener {
 //	private static final String LOG_NAME = gcTimer.LOG_NAME;
+	private static final String LISTENER_NAME = "gcTimerCommands";
+
+
+
+	@Override
+	public String getName() {
+		return LISTENER_NAME;
+	}
+	@Override
+	public String toString() {
+		return this.getName();
+	}
 
 
 
